0ec9310c18b322a673adfe744a936deac3fb1b2f
wickedside <w1ck3ds1d3@gmail.com>
2022-12-13 21:25:02 +0300

    feat: doRectanglesOverlap

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -129,8 +129,13 @@ function isTriangle(a, b, c) {
  *   { top:20, left:20, width: 20, height: 20 }    =>  false
  *
  */
-function doRectanglesOverlap(/* rect1, rect2 */) {
-  throw new Error('Not implemented');
+function doRectanglesOverlap(rect1, rect2) {
+  return (
+    rect1.top < rect2.top + rect2.height
+    && rect1.top + rect1.height > rect2.top
+    && rect1.left < rect2.left + rect2.width
+    && rect1.left + rect1.width > rect2.left
+  );
 }
 
 /**
