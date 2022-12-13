b7424d9e573f0a0f0776db46b9df0d35809414ec
wickedside <w1ck3ds1d3@gmail.com>
2022-12-13 21:29:58 +0300

    feat: getDigitalRoot

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -294,8 +294,9 @@ function isCreditCardNumber(ccn) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  const sum = num.toString().split('').reduce((acc, item) => acc + +item, 0);
+  return sum > 9 ? getDigitalRoot(sum) : sum;
 }
 
 /**
