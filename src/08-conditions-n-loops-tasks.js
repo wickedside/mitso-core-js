f0616cd9835d6826c6b12f0a880129d17d3f5dde
wickedside <w1ck3ds1d3@gmail.com>
2022-12-13 21:28:18 +0300

    feat: reverseInteger

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -241,8 +241,8 @@ function reverseString(str) {
  *   87354 => 45378
  *   34143 => 34143
  */
-function reverseInteger(/* num */) {
-  throw new Error('Not implemented');
+function reverseInteger(num) {
+  return +num.toString().split('').reverse().join('');
 }
 
 /**
