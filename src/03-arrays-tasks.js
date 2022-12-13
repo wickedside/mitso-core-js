fc620d26c2b034de561a3bf3007c5f3514d6aeac
wickedside <w1ck3ds1d3@gmail.com>
2022-12-13 21:03:12 +0300

    feat: getSecondItems

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -243,8 +243,8 @@ function getMovingSum(arr) {
  * [ 'a', 'b', 'c' , null ]  => [ "b", null ]
  * [ "a" ] => []
  */
-function getSecondItems(/* arr */) {
-  throw new Error('Not implemented');
+function getSecondItems(arr) {
+  return arr.filter((_, i) => i % 2);
 }
 
 /**
