2f26fc67ee7b7040eb5468dc4449f8f4ec01f167
wickedside <w1ck3ds1d3@gmail.com>
2022-12-13 21:03:39 +0300

    feat: propagateItemsByPositionIndex

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -261,8 +261,8 @@ function getSecondItems(arr) {
  *  [ 'a', 'b', 'c', null ] => [ 'a', 'b','b', 'c','c','c',  null,null,null,null ]
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
-function propagateItemsByPositionIndex(/* arr */) {
-  throw new Error('Not implemented');
+function propagateItemsByPositionIndex(arr) {
+  return arr.reduce((acc, el, i) => acc.concat(Array(i + 1).fill(el)), []);
 }
 
 /**
