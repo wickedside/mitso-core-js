596cff4d78fc6f573eae2d3c8a05956b5760c670
wickedside <w1ck3ds1d3@gmail.com>
2022-12-13 20:49:51 +0300

    feat: unbracketTag

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -139,8 +139,8 @@ function removeFirstOccurrences(str, value) {
  *   '<span>' => 'span'
  *   '<a>' => 'a'
  */
-function unbracketTag(/* str */) {
-  throw new Error('Not implemented');
+function unbracketTag(str) {
+  return str.slice(1, -1);
 }
 
 /**
