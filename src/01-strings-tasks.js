b32a655d63dc16ba29faf84d7a500077fdb152eb
wickedside <w1ck3ds1d3@gmail.com>
2022-12-13 20:47:29 +0300

    feat: extractNameFromTemplate

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -63,9 +63,10 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, John Doe!' => 'John Doe'
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  return value.slice(7, -1);
 }
+
 
 /**
  * Returns a first char of the given string.
