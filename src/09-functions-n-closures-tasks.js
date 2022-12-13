d5af85e4e4443591544b4d7ae610e7cb0c5ee9b8
wickedside <w1ck3ds1d3@gmail.com>
2022-12-13 21:54:47 +0300

    feat: getIdGeneratorFunction

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -185,8 +185,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let current = startFrom;
+  return () => {
+    const result = current;
+    current += 1;
+    return result;
+  };
 }
 
 module.exports = {
