f6878d19cec33bc5a1518be79abce678371d3985
wickedside <w1ck3ds1d3@gmail.com>
2022-12-13 21:17:52 +0300

    feat: processAllPromises

--- src/07-promises-tasks.js
+++ src/07-promises-tasks.js
@@ -52,8 +52,8 @@ function willYouMarryMe(isPositiveAnswer) {
  *    })
  *
  */
-function processAllPromises(/* array */) {
-  throw new Error('Not implemented');
+function processAllPromises(array) {
+  return Promise.all(array);
 }
 
 /**
