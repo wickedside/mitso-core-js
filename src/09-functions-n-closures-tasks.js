46e62a16d8ebf20e4e7b295b1d90d0bec2ae4094
wickedside <w1ck3ds1d3@gmail.com>
2022-12-13 21:38:43 +0300

    feat: getPowerFunction

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -42,8 +42,8 @@ function getComposition(f, g) {
  *   power05(16) => 4
  *
  */
-function getPowerFunction(/* exponent */) {
-  throw new Error('Not implemented');
+function getPowerFunction(exponent) {
+  return (x) => x ** exponent;
 }
 
 /**
