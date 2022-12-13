640ef3a73d9001ec00c948d1802ce22fa7215455
wickedside <w1ck3ds1d3@gmail.com>
2022-12-13 21:06:19 +0300

    feat: sortCitiesArray

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -408,8 +408,13 @@ function toStringList(arr) {
  *      { country: 'Russia',  city: 'Saint Petersburg' }
  *    ]
  */
-function sortCitiesArray(/* arr */) {
-  throw new Error('Not implemented');
+function sortCitiesArray(arr) {
+  return arr.sort((a, b) => {
+    if (a.country === b.country) {
+      return a.city > b.city ? 1 : -1;
+    }
+    return a.country > b.country ? 1 : -1;
+  });
 }
 
 /**
