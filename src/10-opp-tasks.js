f9bfb5c92e8f38d0acb3e2d53d346519d5c25c19
wickedside <w1ck3ds1d3@gmail.com>
2022-12-13 21:54:47 +0300

    feat: getIdGeneratorFunction

--- src/10-opp-tasks.js
+++ src/10-opp-tasks.js
@@ -24,8 +24,9 @@
 class PaginationHelper {
   // The constructor takes in an array of items and a integer indicating how many
   // items fit within a single page
-  constructor(/* collection, itemsPerPage */) {
-    throw new Error('Not implemented');
+  constructor(collection, itemsPerPage) {
+    this.collection = collection;
+    this.itemsPerPage = itemsPerPage;
   }
 
   // returns the number of items within the entire collection
