e247717b6271bc5e5a3b2e8f20d15f1d5bf99bb3
wickedside <w1ck3ds1d3@gmail.com>
2022-12-13 21:02:53 +0300

    feat: getMovingSum

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -228,8 +228,8 @@ function toArrayOfSquares(arr) {
  *   [ 0, 0, 0, 0, 0]         => [ 0, 0, 0, 0, 0]
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
-function getMovingSum(/* arr */) {
-  throw new Error('Not implemented');
+function getMovingSum(arr) {
+  return arr.map((_, i, srcArr) => srcArr.slice(0, i + 1).reduce((a, b) => a + b));
 }
 
 /**
