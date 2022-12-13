6396ad66af54d2e7e09da655e6624dece094f012
wickedside <w1ck3ds1d3@gmail.com>
2022-12-13 21:07:40 +0300

    feat: distinct

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -470,8 +470,8 @@ function getIntervalArray(start, end) {
  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  return [...new Set(arr)];
 }
 
 /**
