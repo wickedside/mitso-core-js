0c717e72ab6a70275b4d2e61cb33a7d2e8cc1ef2
wickedside <w1ck3ds1d3@gmail.com>
2022-12-13 20:57:42 +0300

    feat: isPrime

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -198,8 +198,11 @@ function roundToPowerOfTen(num, pow) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  for (let i = 2, s = Math.sqrt(n); i <= s; i += 1) {
+    if (n % i === 0) return false;
+  }
+  return n > 1;
 }
 
 /**
