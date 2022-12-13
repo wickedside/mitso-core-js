57ed8464bf2570a98f56f4db6f5ded1b4227d280
wickedside <w1ck3ds1d3@gmail.com>
2022-12-13 21:39:22 +0300

    feat: memoize

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -81,8 +81,12 @@ function getPolynom(...args) {
  *   ...
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
-function memoize(/* func */) {
-  throw new Error('Not implemented');
+function memoize(func) {
+  let result;
+  return () => {
+    if (result === undefined) result = func();
+    return result;
+  };
 }
 
 /**
