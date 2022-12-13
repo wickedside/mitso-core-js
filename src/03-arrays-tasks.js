285c71fcb8b5a6e41fcd4279f7fd648761c891bd
wickedside <w1ck3ds1d3@gmail.com>
2022-12-13 21:09:01 +0300

    feat: getElementByIndexes

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -546,8 +546,8 @@ function selectMany(arr, childrenSelector) {
  *   ['one','two','three'], [2]       => 'three'  (arr[2])
  *   [[[ 1, 2, 3]]], [ 0, 0, 1 ]      => 2        (arr[0][0][1])
  */
-function getElementByIndexes(/* arr, indexes */) {
-  throw new Error('Not implemented');
+function getElementByIndexes(arr, indexes) {
+  return indexes.reduce((acc, el) => acc[el], arr);
 }
 
 /**
