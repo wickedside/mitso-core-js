be80a6613a8be3103d4d43cca1a7f03a924cd21d
wickedside <w1ck3ds1d3@gmail.com>
2022-12-13 21:06:50 +0300

    feat: getIdentityMatrix

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -435,8 +435,11 @@ function sortCitiesArray(arr) {
  *           [0,0,0,1,0],
  *           [0,0,0,0,1]]
  */
-function getIdentityMatrix(/* n */) {
-  throw new Error('Not implemented');
+function getIdentityMatrix(n) {
+  return Array.from(
+    { length: n },
+    (_, i) => Array.from({ length: n }, (__, j) => (i === j ? 1 : 0)),
+  );
 }
 
 /**
