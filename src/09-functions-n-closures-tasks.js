e85b66190d6aad5a31f48c2dd39edad89cdacb54
wickedside <w1ck3ds1d3@gmail.com>
2022-12-13 21:39:01 +0300

    feat: getPolynom

--- src/09-functions-n-closures-tasks.js
+++ src/09-functions-n-closures-tasks.js
@@ -59,8 +59,12 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  if (args.length === 0) return null;
+  if (args.length === 1) return () => args[0];
+  if (args.length === 2) return (x) => args[0] * x + args[1];
+  if (args.length === 3) return (x) => args[0] * x ** 2 + args[1] * x + args[2];
+  return null;
 }
 
 /**
