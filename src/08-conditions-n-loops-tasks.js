6e17067adc9bd2fa31dfe09b4920bc80a0322a52
wickedside <w1ck3ds1d3@gmail.com>
2022-12-13 21:23:49 +0300

    feat: getFactorial

--- src/08-conditions-n-loops-tasks.js
+++ src/08-conditions-n-loops-tasks.js
@@ -50,8 +50,12 @@ function getFizzBuzz(num) {
  *   5  => 120
  *   10 => 3628800
  */
-function getFactorial(/* n */) {
-  throw new Error('Not implemented');
+function getFactorial(n) {
+  let result = 1;
+  for (let i = 1; i <= n; i += 1) {
+    result *= i;
+  }
+  return result;
 }
 
 /**
